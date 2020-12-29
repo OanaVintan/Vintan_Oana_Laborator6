6c994141a6710079c267d056e7c39a9fb03bd74c
OANA-MARIA VINŢAN <oana.maria.vintan@stud.ubbcluj.ro>
2020-12-29 15:43:54 +0200

    Add project files.

---
+++ Data/Vintan_Oana_Laborator6Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Vintan_Oana_Laborator6.Models;
+
+namespace Vintan_Oana_Laborator6.Data
+{
+    public class Vintan_Oana_Laborator6Context : DbContext
+    {
+        public Vintan_Oana_Laborator6Context (DbContextOptions<Vintan_Oana_Laborator6Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Vintan_Oana_Laborator6.Models.Book> Book { get; set; }
+    }
+}
---
+++ Pages/Books/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Vintan_Oana_Laborator6.Data;
+using Vintan_Oana_Laborator6.Models;
+
+namespace Vintan_Oana_Laborator6.Pages.Books
+{
+    public class IndexModel : PageModel
+    {
+        private readonly Vintan_Oana_Laborator6.Data.Vintan_Oana_Laborator6Context _context;
+
+        public IndexModel(Vintan_Oana_Laborator6.Data.Vintan_Oana_Laborator6Context context)
+        {
+            _context = context;
+        }
+
+        public IList<Book> Book { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Book = await _context.Book.ToListAsync();
+        }
+    }
+}
